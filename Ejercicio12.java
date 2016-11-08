9095996a4e86a42c032697831ba239288a01c161
Raúl <1raulmartin1@gmail.com>
2016-11-08 09:49:50 +0100

    Correccion de errores fundamentalmente

--- Ejercicio12.java
+++ Ejercicio12.java
@@ -1,20 +1,23 @@
 /**
- * Serie Fibonaci
+ * Serie Fibonacci
  * @author Raul Martin
  */
 public class Ejercicio12 {
 	public static void main(String[] args) {
 		System.out.print("Indica la cantidad de los numeros que se mostraran de la serie fibonaci: ");
 		int n = Integer.parseInt(System.console().readLine());
-		int serie = 0;
-		int fibonaci = 1;
-		int cantidad = 0;
+		long serie = 0;
+		long fibonacci = 1;
+		int cantidad = 1;
 		while(n >= cantidad) {
-			fibonaci+=serie;
-			System.out.println(serie);
-			System.out.println(fibonaci);
-			serie+=fibonaci;
+			fibonacci+=serie;
+			System.out.print(" "+serie);
 			cantidad++;
+			if(n >= cantidad) {
+				System.out.print(" "+fibonacci);
+				serie+=fibonacci;
+				cantidad++;
+			}
 		}
 	}
 }
---
+++ Ejercicio16.java
@@ -0,0 +1,24 @@
+/**
+ * Saber si un numero es primo
+ * @author Raul Martin
+ */
+public class Ejercicio16 {
+	public static void main(String[] args) {
+		System.out.print("Introduce un numero para saber si es primo: ");
+		int primo = Integer.parseInt(System.console().readLine());
+		int resultado = 0;
+		boolean esPrimo = true;
+		for(int n=2; n < primo; n++){
+			resultado = primo % n;
+			if(resultado == 0){
+				esPrimo=false;
+			}
+		}
+		
+		if(esPrimo){
+			System.out.println("el numero es primo");
+		}else{
+			System.out.println("el numero no es primo");
+	}
+}
+}
